0e7ec1dbee8377c3ee95b3a20a63c0fff19b7730
Dattatraya Walunj <drwalunj.2010@gmail.com>
2026-04-22 13:11:06 +0530

    Added Creating Superuser into the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,17 @@ done
 echo "PostgreSQL Started"
 
 echo "Running Migrations"
+python manage.py migrate_schemas --shared
+
+python manage.py makemigrations customer
+python manage.py migrate customer
+
+python manage.py makemigrations users
+python manage.py migrate users
+
+python manage.py makemigrations project_management
+python manage.py migrate project_management
+
 python manage.py migrate
 
 echo "Creating Superuser"
